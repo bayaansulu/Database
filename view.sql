e84a9fcde4e80864a4d85ac18446ecd1c901cae4
bayaansulu <bssbayan@gmail.com>
2024-07-24 10:50:31 +0500

    Delete view.sql

--- view.sql
+++
@@ -1,9 +0,0 @@
-Create view Comments_View AS 
-Select Comment_ID, Product_ID, Rating  
-From Comment  
-Where Rating > 5 
-Select * from Comment 
-
-Update Comments_View  
-Set rating = 9 
-Where Product_ID = ‘411’
